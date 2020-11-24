39fac4c63830b0395f187331e8fb7847c3416f29
Jimmy <m17702188864@163.com>
2020-11-24 14:23:54 +0800

    init

---
+++ JiaJieMingYue/JiaJieMingYue/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  JiaJieMingYue
+//
+//  Created by Jimmy on 2020/11/11.
+//  Copyright © 2020 Jimmy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
---
+++ JiaJieMingYue/JiaJieMingYue/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  JiaJieMingYue
+//
+//  Created by Jimmy on 2020/11/11.
+//  Copyright © 2020 Jimmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
